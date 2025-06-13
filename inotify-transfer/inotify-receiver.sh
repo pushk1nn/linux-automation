824301f77a7e8c1071e6123cfd76c8acd2fd449c
Maxim <mvkochnev42@gmail.com>
2025-06-12 21:45:01 -0400

    oops

--- inotify-transfer/inotify-receiver.sh
+++ inotify-transfer/inotify-receiver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+WATCH_DIR="."
+
 inotifywait -m -e close_write --format "%f" "$WATCH_DIR" | while read filename; do
 	if [[ "$filename" == *.epub ]]; then
 		calibredb add "$filename" --with-library /opt/calibre-web
