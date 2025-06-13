3e9094eae2ae3067b9cf2f2d143f0e803bf7d624
Maxim <mvkochnev42@gmail.com>
2025-06-12 20:45:30 -0400

    edited rsync script. it's scuffed but fiwb

--- inotify-transfer/inotify-rsync.sh
+++ inotify-transfer/inotify-rsync.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-WATCH_DIR=""
+WATCH_DIR="."
+USER=""
+REMOTE_HOST=""
+REMOTE_DIR=""
+FILETYPE=""
 
-inotifywait -m -e close_write --format '%f' "$WATCH_DIR" ; while read filename; do
-	if [[ "$filename" == *.csv ]]; then
-		echo "Detected new file: $filename"
-
-	fi
+inotifywait -m -e close_write --format '%f' "$WATCH_DIR" | while read filename; do
+        if [[ "$filename" == *."$FILETYPE" ]]; then
+                rsync -avz *."$FILETYPE" "$USER"@"$REMOTE_HOST":"$REMOTE_DIR"
+        fi
 done
