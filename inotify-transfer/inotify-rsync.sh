33745691094985f04aba1aad422a330026968167
Maxim <mvkochnev42@gmail.com>
2025-06-12 21:35:55 -0400

    recursion & servicing

--- inotify-transfer/inotify-rsync.sh
+++ inotify-transfer/inotify-rsync.sh
@@ -6,7 +6,7 @@ REMOTE_HOST=""
 REMOTE_DIR=""
 FILETYPE=""
 
-inotifywait -m -e close_write --format '%f' "$WATCH_DIR" | while read filename; do
+inotifywait -r -m -e close_write --format '%f' "$WATCH_DIR" | while read filename; do
         if [[ "$filename" == *."$FILETYPE" ]]; then
                 rsync -avz *."$FILETYPE" "$USER"@"$REMOTE_HOST":"$REMOTE_DIR"
         fi
