6a1e6184141d31719acd3825e6049592dad90965
korben <korben@siftworkstation.local>
2025-09-15 00:01:29 +0000

    update

--- system-info.sh
+++ system-info.sh
@@ -22,6 +22,14 @@ df=$(df -h | awk '$6=="/" {print "Disk " $1 ", Size " $2 ", Used " $3 ", Mounted
 hostname=$(hostname)
 domain=$(hostname -d)
 
-printf "[+] Architecture: %s\n[+] Memory: %s\n[+] Filesystem: %s\n[+] Partitions: \n%s\n[+] Hostname: %s\n[+] Domain: %s" "$arch" "$mem" "$df" "$(lsblk)" "$hostname" "$domain"
+printf "[+] Architecture: %s\n[+] Memory: %s\n[+] Filesystem: %s\n[+] Partitions: \n%s\n[+] Hostname: %s\n[+] Domain: %s\n" "$arch" "$mem" "$df" "$(lsblk)" "$hostname" "$domain"
+
+echo -e "\n----\tNETWORKING\t----"
+
+mac=$(ip -c a | awk '/^[0-9]+:/ {gsub(/:/, "", $2); iface=$2 } /link\/ether/ {print iface, $2}')
+ip=$(ip -c a | awk '/^[0-9]+:/ {gsub(/:/, "", $2); iface=$2 } $1=="inet" {gsub(/\/[0-9]{1,2}/, "", $2); print iface, $2}')
+promisc=$(ip -d link show | awk '/^[0-9]+:/ {gsub(/:/, "", $2); iface=$2 } match($0, /\<promiscuity\>[[:space:]]+([0-9]+)/, arr) {print iface, "is",  arr[1]==0 ? "not" : "\b", "promiscuous"}')
+
+printf "[+] MAC: \n%s\n[+] IP: \n%s\n[+] Promiscuous?: \n%s\n[+] Connections: \n%s\n" "$mac" "$ip" "$promisc" "f"
 
 echo -e "\n"
